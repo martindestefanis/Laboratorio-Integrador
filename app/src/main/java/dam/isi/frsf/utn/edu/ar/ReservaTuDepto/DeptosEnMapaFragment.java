463d7d3ef3d3620a4783775eb8c0acf6dec4ed85
martindestefanis <tinchodestefanis1@gmail.com>
2018-12-17 19:57:00 -0300

    Ver depto en mapa empezado

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/ReservaTuDepto/DeptosEnMapaFragment.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/ReservaTuDepto/DeptosEnMapaFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.List;
@@ -24,6 +25,8 @@ public class DeptosEnMapaFragment extends Fragment {
     private CiudadDAO ciudadDAO;
     private List<Ciudad> ciudades;
     private ArrayAdapter<Ciudad> adapterCiudad;
+    private Button btnVerEnMapa;
+    private Ciudad item;
 
     public DeptosEnMapaFragment() {
         // Required empty public constructor
@@ -35,6 +38,7 @@ public class DeptosEnMapaFragment extends Fragment {
 
         cmbCiudad = (Spinner) v.findViewById(R.id.spinnerCiudad);
         ciudadDAO = MyDatabase.getInstance(this.getActivity()).getCiudadDAO();
+        btnVerEnMapa = (Button) v.findViewById(R.id.btnVerEnMapa);
 
         Runnable r = new Runnable() {
             @Override
@@ -45,34 +49,54 @@ public class DeptosEnMapaFragment extends Fragment {
                     public void run() {
                         adapterCiudad = new ArrayAdapter<Ciudad>(getContext(), android.R.layout.simple_spinner_item, ciudades);
                         cmbCiudad.setAdapter(adapterCiudad);
-                        cmbCiudad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                            @Override
-                            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                                Ciudad item = (Ciudad) parent.getItemAtPosition(pos);
-                                Fragment f = new MapaFragment();
-                                Bundle args = new Bundle();
-                                // setear los parametros tipo_mapa y idDepto en el Bundle args
-                                args.putInt("tipo_mapa", 3);
-                                args.putInt("idCiudad", item.getId()); //VER
-                                f.setArguments(args);
-                                getActivity().getSupportFragmentManager()
-                                        .beginTransaction()
-                                        .replace(R.id.contenido, f)
-                                        .addToBackStack(null)
-                                        .commit();
-                            }
 
-                            @Override
-                            public void onNothingSelected(AdapterView<?> parent) {
 
-                            }
-                        });
                     }
                 });
             }
         };
         Thread t = new Thread(r);
         t.start();
+
+        cmbCiudad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                item = (Ciudad) parent.getItemAtPosition(pos);
+                Fragment f = new MapaFragment();
+                Bundle args = new Bundle();
+                // setear los parametros tipo_mapa y idDepto en el Bundle args
+                args.putInt("tipo_mapa", 3);
+                args.putInt("idCiudad", item.getId()); //VER
+                f.setArguments(args);
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contenido, f)
+                        .addToBackStack(null)
+                        .commit();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnVerEnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment f = new MapaFragment();
+                Bundle args = new Bundle();
+                // setear los parametros tipo_mapa y idDepto en el Bundle args
+                args.putInt("tipo_mapa", 3);
+                args.putInt("idCiudad", item.getId()); //VER
+                f.setArguments(args);
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contenido, f)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
 
         return v;
     };
